c35a11bdf6f34c01adaa01e6cefe04fc84e5538f
Xyrfo <rofo.anggi18@gmail.com>
2026-02-24 19:07:46 +0700

    Penambahan Route, Controller, dan View.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,48 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/greeting', [
+    WelcomeController::class,
+    'greeting'
+]);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
+
+Route::get('/', HomeController::class);
+
+Route::get('/about', AboutController::class);
+
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Rifo Anggi B D ' . $name;
+});
+
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Rifo Anggi B D ' . $name;
 });
