bcf6209fb5e2ec865cd6977b4d1c2357bb1a7ce7
Xyrfo <rofo.anggi18@gmail.com>
2026-02-24 21:45:57 +0700

    Week-2

---
+++ Week-2/POS/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function __invoke($id)
+    {
+        return "Ini adalah halaman artikel dengan ID: $id";
+    }
+}
